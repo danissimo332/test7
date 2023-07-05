78b206d930655e5d14376f812ee75d01293292d4
danissimo <ovseenko_daniil@mail.ru>
2023-07-05 18:27:32 +0300

    v3

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
+import os
+
 from telebot.async_telebot import AsyncTeleBot
 from telebot.types import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
 
-bot = AsyncTeleBot('6311823145:AAEEgxS0m_XjlBV0yQus_eizz7NITbAooVY')
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+TOKEN=os.getenv('TOKEN')
+bot=AsyncTeleBot(TOKEN)
+bot=AsyncTeleBot(TOKEN, parse_mode='HTML')#курсив
 
 @bot.message_handler(commands=['help','start'])
 async def send_welcome(message):
     await bot.reply_to(message, 'Приветствую тебя новый пользователь!')
     print(message)
+
+@bot.message_handler(commands=['finish'])
+async def send_welcome(message):
+    await bot.reply_to(message, 'Конец')
+
 
 @bot.message_handler(commands=['a'])
 async def send_welcome(message):
@@ -37,7 +48,7 @@ async def send_welcome(message):
 @bot.message_handler(commands=['v'])#фото
 async def send_welcome(message):
     chat_id = message.from_user.id
-    await bot.send_photo(chat_id), #'https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0', caption='Заголовок')
+    await bot.send_photo(chat_id,'https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0', caption='Заголовок')
 
 @bot.message_handler(commands=['y'])#клавиатура
 async def send_welcome(message):
@@ -70,9 +81,76 @@ async def send_welcome(message):
     markup.add(button3)
     await bot.send_message(chat_id, 'Первый вариант кнопок', reply_markup=markup)
 
+@bot.message_handler(commands=['m'])#таймер
+async def send_welcome(message):
+    chat_id=message.from_user.id
+    bot_message=await bot.send_message(chat_id, 'Начался таймер 5 секунд')
+    for i in range (1,6):
+        await asyncio.sleep(1)
+        await bot.edit_message_text(f'{5-i} секунд осталось', chat_id, bot_message.id)
+    await bot.delete_message(chat_id, bot_message.id)
+
+@bot.message_handler(commands=['p'])#удалить
+async def send_welcome(message):
+    chat_id=message.from_user.id
+    await bot.delete_message(chat_id, message.id)
+
+@bot.message_handler(commands=['h'])#курсив
+async def send_welcome(message):
+    chat_id=message.from_user.id
+    await bot.send_message(chat_id, '<i>текст</i>')
+
+@bot.message_handler(commands=['zt'])#жирный шрифт
+async def send_welcome(message):
+    chat_id=message.from_user.id
+    await bot.send_message(chat_id, '<b>текст</b>')
+
+@bot.message_handler(commands=['zp'])#подчеркивание
+async def send_welcome(message):
+    chat_id=message.from_user.id
+    await bot.send_message(chat_id, '<u>текст</u>')
+
+@bot.message_handler(commands=['zt'])#подчеркивание
+async def send_welcome(message):
+    chat_id=message.from_user.id
+    await bot.send_message(chat_id, '<u>текст</u>')
+
+@bot.message_handler(commands=['zz'])#зачеркивание
+async def send_welcome(message):
+    chat_id=message.from_user.id
+    await bot.send_message(chat_id, '<s>текст</s>')
+
+@bot.message_handler(commands=['zm'])#моноширныйшрифт
+async def send_welcome(message):
+    chat_id=message.from_user.id
+    await bot.send_message(chat_id, '<code>текст</code>')
+
+@bot.message_handler(commands=['zm2'])#моноширныйшрифт c сохранение пробелов и переносов
+async def send_welcome(message):
+    chat_id=message.from_user.id
+    await bot.send_message(chat_id, '<pre>текст</pre>')
+
+@bot.message_handler(commands=['sp'])#скрывает под спойлер
+async def send_welcome(message):
+    chat_id=message.from_user.id
+    await bot.send_message(chat_id, '<tg-spoiler>текст</tg-spoiler>')
+
+@bot.message_handler(commands=['cc'])#гиперсылка на текст
+async def send_welcome(message):
+    chat_id=message.from_user.id
+    await bot.send_message(chat_id, '<a href="https://www.youtube.com/?gl=RU&hl=ru">текст</a>')
+
+@bot.message_handler(commands=['cp'])#скрывает под спойлер
+async def send_welcome(message):
+    chat_id=message.from_user.id
+    await bot.send_message(chat_id, '<a href="tg://user?id=6159034023">inline mention of a user</a>')
 
 
 
+
+@bot.message_handler(commands=['finish'])
+async def send_welcome(message):
+    await bot.reply_to(message, 'Конец')
 
 
 
@@ -88,11 +166,6 @@ async def echo_message(message):
         await bot.reply_to(message, 'i')
     else:
         await bot.reply_to(message, 'я не понимаю')
-
-
-@bot.message_handler(commands=['finish'])
-async def send_welcome(message):
-    await bot.reply_to(message, 'Конец')
 
 
 import asyncio
